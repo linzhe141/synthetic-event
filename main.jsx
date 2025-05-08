03c51a9663dce6bfbda9f304bdfea72e65c177f4
linzhe141 <1572213544@qq.com>
2025-05-08 13:32:10 +0800

    chore: update

--- main.jsx
+++ main.jsx
@@ -45,3 +45,43 @@ btn.addEventListener('click', () => {
   const logWrapper = document.getElementById('log-wrapper')
   logWrapper.innerHTML = ''
 })
+
+const codeDisplay = document.getElementById('code-display')
+function escapeHTML(strings) {
+  const raw = strings[0]
+
+  return raw
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+}
+codeDisplay.innerHTML = escapeHTML`
+const elements = (
+  <div
+    onClick={() => console.log('parent onClick')}
+    onClickCapture={() => console.log('parent onClickCapture')}
+  >
+    <div
+      onClick={(e) => {
+        e.stopPropagation()
+        console.log('111 onClick')
+      }}
+      onClickCapture={(e) => {
+        console.log('111 onClickCapture')
+      }}
+    >
+      111
+    </div>
+    <div
+      onClick={() => console.log('222 onClick')}
+      onClickCapture={() => console.log('222 onClickCapture')}
+    >
+      222
+    </div>
+  </div>
+)
+const root = createRoot(document.getElementById('root'))
+root.render(elements)
+`
