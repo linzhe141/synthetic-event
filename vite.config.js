d14bef35665cc1c3818012bceaa6d8d6f6e8d7bc
linzhe141 <1572213544@qq.com>
2025-05-08 09:31:45 +0800

    refactor: simplify babel plugin configuration

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import babel from '@rollup/plugin-babel'
 
 export default defineConfig({
-  plugins: [
-    babel({
-      babelHelpers: 'bundled',
-      extensions: ['.js', '.jsx'],
-      include: ['./**/*'],
-      presets: [['@babel/preset-react', { pragma: 'h' }]],
-    }),
-  ],
+  plugins: [babel()],
 })
